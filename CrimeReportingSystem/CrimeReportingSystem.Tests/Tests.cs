f093ee6bf20c80cc940a30559f8220fd2eaa90a4
Gowtham C <146124693+GowthamC06@users.noreply.github.com>
2024-12-16 19:53:15 +0530

    Add files via upload

---
+++ CrimeReportingSystem/CrimeReportingSystem.Tests/Tests.cs
@@ -0,0 +1,36 @@
+﻿using CrimeReportingSystemAPP.Entity;
+using CrimeReportingSystemAPP.dao;
+using NUnit.Framework;
+
+namespace CrimeReportingSystem.Tests
+{
+    public class Tests
+    {
+        ICrimeAnalysisService service = new CrimeAnalysisService();
+
+        [Test]
+        public void TestToAddIncident()
+        {
+            Incidents incident = new Incidents()
+            {
+                IncidentType = "Homicide",
+                IncidentDate = DateTime.Now,
+                Location = "Test location",
+                Description = "Test description",
+                Status = "Open",
+                AgencyId = 1
+            };
+            int addIncidentStatus = service.AddIncidents(incident);
+            Assert.That(addIncidentStatus, Is.GreaterThan(0), "Incident ID should be greater than 0");
+        }
+
+        [Test]
+        public void TestToUpdateIncident()
+        {
+            int incidentId = 2;
+            string status = "Closed";
+            int updateIncidentStatus = service.UpdateIncidentStatus(incidentId, status);
+            Assert.That(updateIncidentStatus, Is.EqualTo(1));
+        }
+    }
+}
